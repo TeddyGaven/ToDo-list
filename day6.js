fbd05b809c10bca6eab59518f2e6dac217ad26d3
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2022-09-19 21:28:52 +0530

    Add files via upload

--- day6.js
+++ day6.js
@@ -1,24 +1,43 @@
 a = document.getElementById('txt1')
 add = document.getElementById('btn1');
 list = document.getElementById('container');
+save = document.getElementById('btn2')
 
 
 
 add.addEventListener('click', function(){ 
+    task = a.value
+    localStorage.setItem('Name',task)
+    a.value = ""
+})
+
+save.addEventListener('click', function(){
     var para = document.createElement('p')
     para.classList.add('para')
-    para.innerText = a.value;
-    list.appendChild(para);
-    a.value = "";
-
+    para.innerText = JSON.stringify(localStorage.getItem("Name"))
+    list.appendChild(para)
 
     para.addEventListener('click', function(){
         para.style.textDecoration = 'line-through';
 
         para.addEventListener('click', function(){
             para.style.textDecoration = ""
-        })
+
+        }) 
     })
+
+          
+    para.addEventListener('dblclick', function(){
+         list.removeChild(para);     
+    })         
+            
+})
+           
+        
+    
+
+
+
     //para.addEventListener('click', function(){If(para.style.textDecoration = "")
    // {
     // para.style.textDecoration = 'line-through'
@@ -29,12 +48,9 @@ add.addEventListener('click', function(){
    // }
    // }) 
 
-    para.addEventListener('dblclick', function(){
-       list.removeChild(para);     
-    })
+   
 
 
-})
   
 
 
